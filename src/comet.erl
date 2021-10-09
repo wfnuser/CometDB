4b282c73231f65613a29254bc13d4f86b8c56f8c
wfnuser <wfnuser@126.com>
2021-10-09 17:53:50 +0800

    feat: support basic apis

--- src/comet.erl
+++ src/comet.erl
@@ -2,42 +2,46 @@
 
 -export([
     new_queue/2,
-    enqueue/2,
-    dequeue/1
+    insert/2,
+    fetchN/2,
+    fetchOne/1,
+    rangeDelete/3
 ]).
-
-timestamp() ->
-    {M, S, _} = os:timestamp(),
-    M * 1000000 + S.
 
 new_queue(ClusterFile, QueueName) -> 
     Db = erlfdb:open(ClusterFile),
     {Db, QueueName}.
 
-enqueue(Queue, Value) ->
+insert(Queue, Value) ->
     case Queue of
         {Db, QueueName} ->
-            Key = erlfdb_tuple:pack({timestamp()},QueueName),
+            Key = erlfdb_tuple:pack({uuid:uuid1()},QueueName),
             erlfdb:set(Db, Key, Value)
     end.
+
+fetchOne(Queue) -> fetchN(Queue, 1).
         
-dequeue(Queue) ->
+fetchN(Queue, N) ->
     case Queue of
         {Db, QueueName} -> 
             Opts = [
-                % {snapshot, true},
-                % {reverse, true},
-                % {streaming_mode, exact},
-                {limit, 1}
+                {limit, N}
             ],
             KVs = erlfdb:get_range_startswith(Db, QueueName, Opts),
             lists:map(
                 fun({K, V}) ->
-                    io:format("key: ~p value: ~p ~n", [K, V])
+                    {UUID} = erlfdb_tuple:unpack(K, QueueName),
+                    {UUID, V}
                 end,
                 KVs
             )
     end.
+
+rangeDelete(Queue, Start, End) ->
+    case Queue of
+        {Db, QueueName} -> 
+            erlfdb:clear_range(Db, erlfdb_tuple:pack({Start}, QueueName), erlfdb_tuple:pack({End}, QueueName))
+    end.    
 
 
 
