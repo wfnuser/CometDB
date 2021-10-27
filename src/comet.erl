3c393152916234547ab1f186b6397c72c37e999c
wfnuser <wfnuser@126.com>
2021-10-27 15:13:20 +0800

    fix: gen_server

--- src/comet.erl
+++ src/comet.erl
@@ -21,18 +21,15 @@ stop() -> gen_server:call(?MODULE, stop).
 start_link() -> gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 init([]) -> {ok, ets:new(?MODULE, [])}.
 
-handle_call({new_queue, ClusterFile, QueueName}, _From, _State) -> comet_queue:new_queue(ClusterFile, QueueName);
-handle_call({insert, Queue, Value}, _From, _State) -> comet_queue:insert(Queue, Value);
-handle_call({batch_insert, Queue, Values}, _From, _State) -> comet_queue:batch_insert(Queue, Values);
-handle_call({fetch_one, Queue}, _From, _State) -> comet_queue:fetch(Queue, 1);
-handle_call({fetch, Queue, N}, _From, _State) -> comet_queue:fetch(Queue, N);
-handle_call({delete, Queue, Key}, _From, _State) -> comet_queue:delete(Queue, Key);
-handle_call({range_delete, Queue, Start, End}, _From, _State) -> comet_queue:range_delete(Queue, Start, End).
+handle_call({new_queue, ClusterFile, QueueName}, _From, State) -> {reply, comet_queue:new_queue(ClusterFile, QueueName), State};
+handle_call({insert, Queue, Value}, _From, State) -> {reply, comet_queue:insert(Queue, Value), State};
+handle_call({batch_insert, Queue, Values}, _From, State) -> {reply, comet_queue:batch_insert(Queue, Values), State};
+handle_call({fetch_one, Queue}, _From, State) -> {reply, comet_queue:fetch(Queue, 1), State};
+handle_call({fetch, Queue, N}, _From, State) -> {reply, comet_queue:fetch(Queue, N), State};
+handle_call({delete, Queue, Key}, _From, State) -> {reply, comet_queue:delete(Queue, Key), State};
+handle_call({range_delete, Queue, Start, End}, _From, State) -> {reply, comet_queue:range_delete(Queue, Start, End), State}.
 
 handle_cast(_Msg, State) -> {noreply, State}.
-
 handle_info(_Info, State) -> {noreply, State}.
-
 code_change(_OldVsn, State, _Extra) -> {ok, State}.
-
 terminate(_Reason, _State) -> ok.
