7a711f7a61363d8a5df7dddb80b466394bb467d9
wfnuser <wfnuser@126.com>
2021-10-28 14:47:48 +0800

    feat: support apis functions

--- src/comet.erl
+++ src/comet.erl
@@ -1,29 +1,61 @@
 -module(comet).
 -behaviour(gen_server).
+
 -export([start_link/0]).
 
--export([start/0, stop/0]).
+-export([start/0
+        , stop/0
+        , new_queue/2
+        , insert/2
+        , batch_insert/2
+        , fetch_one/1
+        , fetch/2
+        , delete/2
+        , range_delete/3
+        ]).
 
--export([
-    init/1,
-    handle_call/3,
-    handle_cast/2,
-    handle_info/2,
-    terminate/2,
-    code_change/3
+-export([ init/1
+        , handle_call/3
+        , handle_cast/2
+        , handle_info/2
+        , terminate/2
+        , code_change/3
 ]).
 
 -define(SERVER, comet).
 
 start() -> 
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
 stop() -> 
     gen_server:call(?MODULE, stop).
 
 start_link() -> 
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
 init([]) -> 
     {ok, ets:new(?MODULE, [])}.
+
+new_queue(ClusterFile, QueueName) ->
+    gen_server:call(?MODULE, {new_queue, ClusterFile, QueueName}).
+
+insert(Queue, Value) ->    
+    gen_server:call(?MODULE, {insert, Queue, Value}).
+
+batch_insert(Queue, Values) ->
+    gen_server:call(?MODULE, {batch_insert, Queue, Values}).
+
+fetch_one(Queue) ->
+    gen_server:call(?MODULE, {fetch_one, Queue}).
+
+fetch(Queue, N) ->
+    gen_server:call(?MODULE, {fetch, Queue, N}).
+
+delete(Queue, Key) ->
+    gen_server:call(?MODULE, {delete, Queue, Key}).
+
+range_delete(Queue, Start, End) ->
+    gen_server:call(?MODULE, {range_delete, Queue, Start, End}).
 
 handle_call({new_queue, ClusterFile, QueueName}, _From, State) -> 
     {reply, comet_queue:new_queue(ClusterFile, QueueName), State};
@@ -54,5 +86,5 @@ handle_info(_Info, State) ->
 
 code_change(_OldVsn, State, _Extra) -> 
     {ok, State}.
-    
+
 terminate(_Reason, _State) -> ok.
